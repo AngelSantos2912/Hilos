49ea22fb630d9824324f9a76aac774ce801e5ffa
AngelSantos2912 <angelleonardo975533406@gmail.com>
2024-09-29 09:09:07 -0500

    xd

---
+++ src/test/java/pe/edu/cibertec/SW2_Async_Hilos/Sw2AsyncHilosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.SW2_Async_Hilos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sw2AsyncHilosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
